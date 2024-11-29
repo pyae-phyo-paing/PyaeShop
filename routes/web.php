590096cd2f7dfe66ff054f50956508becd1688fa
pyae-phyo-paing <paing.mdy.edu@gmail.com>
2024-11-29 21:42:47 +0630

    laravel project lesson nov 29, 2024

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,10 @@ Route::get('/', [App\Http\Controllers\FrontController::class, 'shop'])->name('sh
 
 Route::get('/shop-item/{id}',[App\Http\Controllers\FrontController::class, 'shopItem'])->name('shop-item');
 
+Route::group(['prefix'=>'backend','as'=>'backend.'],function(){
+    Route::get('/',[App\Http\Controllers\Admin\DashboardController::class, 'index'])->name('dashboard');
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
